d3c9d53dc1fac15d4df65463ee5c3119b12c5f4e
Jeet Sharma <jitendra909@gmail.com>
2022-02-21 21:00:41 +0530

    comments

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 const App: React.FC = (props: any) => {
+    // this is an App Component.
     return (
         <div className='app'>
             <h1>Typescript React Webpack Boilerplate</h1>
--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,3 +1,4 @@
+// all the constants will be kept in this file.
 export const constants: any = {
     UPDATE_LOADING_STATUS: 'UPDATE_LOADING_STATUS'
 };
